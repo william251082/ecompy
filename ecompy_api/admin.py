2cb5a5ea4f766c719415b404937d36031f4c5015
william <william.del.rosario@iceshop.nl>
2019-11-01 21:14:59 +0100

    Changed folder structure

--- ecompy_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Comment, User, Product
-
-admin.site.register(Comment)
-admin.site.register(Product)
-admin.site.register(User)
--- ecompy_api/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class EcompyApiConfig(AppConfig):
-    name = 'ecompy_api'
-
-    def ready(self):
-        from ecompy_api.admin import Comment, Product, User
